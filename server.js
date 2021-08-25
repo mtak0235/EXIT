aac554a448ca7d6ebabca85261da40776596768b
Min Kyeong - Tak <48946398+mtak0235@users.noreply.github.com>
2021-08-25 23:13:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var cors = require('cors');
 var app = express();
-
+var userRouter = require('./route/sign');
+var boardRouter = require('./route/board');
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-
 app.use(cookieParser());
 app.use(session({
     secret: 'my key',
@@ -18,20 +18,13 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-
 app.use('/static', express.static('docs'));
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-var userRouter = require('./route/sign');
-var boardRouter = require('./route/board');
-
 app.use("/", userRouter);
 app.use("/sign", userRouter);
 app.use("/write", boardRouter);
-
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port' + app.get('port'));
 });
